0427bf5870ef5d4f7135851910a1d90d805a0728
dwaydwaydway <tim8733123b@gmail.com>
2018-05-30 13:15:32 +0800

    update

--- src/CounterServer.java
+++ src/CounterServer.java
@@ -6,24 +6,20 @@ import java.net.Socket;
 
 public class CounterServer {
 	public enum station {
-		Nangang, Taipei, Banqiao, Taoyuan, Hsinchu, Miaoli, Taichung, Tainan, Changhua, Yunlin, Chiayi, Zuoying
+		Nangang, Taipei, Banqiao, Taoyuan, Hsinchu, Miaoli, 
+		Taichung, Tainan, Changhua, Yunlin, Chiayi, Zuoying
 	}
-
 	public enum type {
 		normal, child, elder, disable
 	}
-
 	SearchCar SearchCar;
-	static NormalBooking nb = new NormalBooking();
-
+	
 	public void listen() throws ClassNotFoundException {
 		try {
-
 			ServerSocket server = new ServerSocket(3336);
 			while (true) {
 				System.out.println("接受連線中");
 				Socket socket = server.accept();
-				System.out.println("connected");
 				socket.setSoTimeout(15000);
 				AddThread add = new AddThread(socket);
 				add.start();
@@ -37,7 +33,8 @@ public class CounterServer {
 	class AddThread extends Thread {
 		private Messenger msg;
 		Socket socket;
-
+		ObjectOutputStream os = null;
+		ObjectInputStream is = null;
 		public AddThread(Socket sk) throws IOException, ClassNotFoundException {
 			socket = sk;
 		}
@@ -61,33 +58,6 @@ public class CounterServer {
 				}
 				
 			
->>>>>>> refs/heads/master
-		}
-	}
-
-	private static void sendObject(ObjectOutputStream oos, Messenger msg) throws IOException {
-		oos.writeObject(msg);
-		oos.flush();
-		oos.close();
-	}
-
-	private static void returnMessage(Messenger msg) {
-		Socket cs = null;
-		try {
-			cs = new Socket("127.0.0.1", 8080);
-		} catch (IOException e) {
-			e.printStackTrace();
-			ObjectOutputStream oos = null;
-			try {
-				oos = new ObjectOutputStream(cs.getOutputStream());
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			}
-			try {
-				sendObject(oos, msg);
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
 		}
 	}
 
